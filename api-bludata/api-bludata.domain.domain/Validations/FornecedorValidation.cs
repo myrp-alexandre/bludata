f944d0cfe3a5c523ecf34b6bbc159bfa7c0a16b6
estevaobraga <estevo.braga@gmail.com>
2019-05-26 12:02:40 -0300

    Fornecedor Validation

---
+++ api-bludata/api-bludata.domain.domain/Validations/FornecedorValidation.cs
@@ -0,0 +1,16 @@
+﻿using api_bludata.domain.domain.models;
+using FluentValidation;
+
+namespace api_bludata.domain.domain.Validations
+{
+    public class FornecedorValidation : AbstractValidator<fornecedor>
+    {
+        public FornecedorValidation()
+        {
+            RuleFor(x => x.empresa.estado.sigla)
+                .Equal("PR")
+                .WithMessage("Número da agência é obrigatório");
+
+        }
+    }
+}
